2bf5626d4c6c11c6394b7f7ea21441db8155bd62
arithmetic1728 <liusijun1985@gmail.com>
2021-02-05 02:39:05 -0800

    update

--- google-cloud-build/src/test/java/com/google/cloud/devtools/cloudbuild/v1/SampleTest.java
+++ google-cloud-build/src/test/java/com/google/cloud/devtools/cloudbuild/v1/SampleTest.java
@@ -18,12 +18,14 @@ package com.google.cloud.devtools.cloudbuild.v1;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.core.GoogleCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.devtools.cloudbuild.v1.CloudBuildClient.ListBuildTriggersPagedResponse;
 import com.google.cloudbuild.v1.BuildTrigger;
 import com.google.common.collect.Lists;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 
@@ -38,10 +40,18 @@ public class SampleTest {
     return CloudBuildSettings.newBuilder().setCredentialsProvider(provider).build();
   }
 
+  public CloudBuildSettings createCustomScopeSetting() throws Exception {
+    List<String> scopes = new ArrayList<>();
+    scopes.add("https://www.googleapis.com/auth/cloud-platform");
+    CredentialsProvider provider = GoogleCredentialsProvider.newBuilder().setScopesToApply(scopes).build();
+    return CloudBuildSettings.newBuilder().setCredentialsProvider(provider).build();
+  }
+
   @Test
   public void myTest() throws Exception {
     // CloudBuildSettings settings = createADCSetting();
-    CloudBuildSettings settings = createFixedCredsSetting();
+    // CloudBuildSettings settings = createFixedCredsSetting();
+    CloudBuildSettings settings = createCustomScopeSetting();
 
     CloudBuildClient client = CloudBuildClient.create(settings);
     String project = "dcatest-281318";
